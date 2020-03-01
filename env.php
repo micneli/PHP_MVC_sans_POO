176a720d3559686c18c853b1b62151fa010edda1
Mineli <monte.nemi@gmail.com>
2020-03-01 18:41:54 +0100

    homepage finished

--- env.php
+++ env.php
@@ -7,7 +7,7 @@
     // $user = "nebojsam";
     // $pass = "s1au/5YBznD6mQ==";
 
-    $projectName = "mvc_sansPOO";
+    $projectName = "PHP_MVC_sans_POO";
     $projectNameLength = strlen($projectName) + 1;
     $base_url = mb_substr($_SERVER['REQUEST_URI'], 0, $projectNameLength);
     
--- header.php
+++ header.php
@@ -9,14 +9,38 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <header id="header" class="">
-        <p>Header here</p>
-        <!-- <ul class="">
-            <li class="">
-                <a href="<?php echo $base_url; ?>">Home</a>
+  <header id="header" class="">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+      <div class="container">
+        <a class="navbar-brand" href="<?php echo $base_url; ?>">WeTransfer2</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="<?php echo $base_url; ?>">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li>
-                exemple de lien que l'on peut faire grâce à l'url rewriting <a href="test">Test</a>
+            <li class="nav-item">
+              <a class="nav-link" href="zip">Zip/send Files</a>
             </li>
-        </ul> -->
-    </header>
+            <!-- <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li> -->
+            <!-- <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown07">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li> -->
+          </ul>
+          <!-- <form class="form-inline my-2 my-md-0">
+            <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+          </form> -->
+        </div>
+      </div>
+    </nav>
+  </header>
