110d95ec33bbe1153cdbffbd471983721daacdf6
Mineli <monte.nemi@gmail.com>
2020-03-05 22:19:59 +0100

    minor syntax updates

--- Views/DownloadView.php
+++ Views/DownloadView.php
@@ -8,8 +8,8 @@ session_start();
     <div class="col-md-6 mx-auto mt-5">
       <div class="card card-body bg-light">
         <h1>Files received</h1>
-        <?php var_dump($name); ?>
-        <p>User <?php echo $_SESSION['username']; ?> sent you a zipped file. Click here to download it
+        <!-- var_dump($name) -->
+        <p>User <?php echo $_SESSION['username']; ?> sent you a zipped file. Click here to download it:
         <a href="uploads/<?php echo $zipPath; ?>" target = "_self">Download</a></p>
       </div>
     </div>
