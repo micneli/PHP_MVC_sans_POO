3a3467cd0a45d0f2a2c635c53f990c85f0894a18
Mineli <monte.nemi@gmail.com>
2020-03-17 16:50:03 +0100

    added favicon.ico

--- env.php
+++ env.php
@@ -6,5 +6,6 @@
 
     $projectName = "PHP_MVC_sans_POO";
     $projectNameLength = strlen($projectName) + 1;
-    $base_url = mb_substr($_SERVER['REQUEST_URI'], 0, $projectNameLength);
+    //$base_url = mb_substr($_SERVER['REQUEST_URI'], 0, $projectNameLength);
+    $base_url = "https://nebojsam.promo-36.codeur.online/PHP_mvc_sanspoo";
     
